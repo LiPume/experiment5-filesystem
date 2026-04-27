51ff4f1eb4f30ee35cbff3ab4579ca23e03d711a
qvgeming <qvgeming@noreply.gitcode.com>
2026-04-27 10:24:23 +0800

    修改文件打开函数

--- src/file.c
+++ src/file.c
@@ -7,25 +7,31 @@
 
 /* 目录中查找文件 */
 static int find_file(unsigned short dir_first, unsigned long length,
-                     const char *name, fcb *out, int *index) {
+                     const char *name, fcb *out, int *index)
+{
 
     int total = length / sizeof(fcb);
     fcb temp;
     char fullname[16];
 
-    for (int i = 0; i < total; i++) {
+    for (int i = 0; i < total; i++)
+    {
         memcpy(&temp, blockaddr[dir_first] + i * sizeof(fcb), sizeof(fcb));
 
-        if (temp.free == 1) continue;
+        if (temp.free == 1)
+            continue;
 
         if (temp.exname[0] != '\0')
             sprintf(fullname, "%s.%s", temp.filename, temp.exname);
         else
             sprintf(fullname, "%s", temp.filename);
 
-        if (strcmp(fullname, name) == 0) {
-            if (out) memcpy(out, &temp, sizeof(fcb));
-            if (index) *index = i;
+        if (strcmp(fullname, name) == 0)
+        {
+            if (out)
+                memcpy(out, &temp, sizeof(fcb));
+            if (index)
+                *index = i;
             return 0;
         }
     }
@@ -34,19 +40,22 @@ static int find_file(unsigned short dir_first, unsigned long length,
 
 /* ===================== 1. 创建文件 ===================== */
 
-int my_create(char *filename) {
+int my_create(char *filename)
+{
     useropen *cur = &openfilelist[curdirid];
     fcb newfile;
     int blk;
     int index;
 
-    if (find_file(cur->first, cur->length, filename, NULL, NULL) == 0) {
+    if (find_file(cur->first, cur->length, filename, NULL, NULL) == 0)
+    {
         printf("文件已存在\n");
         return -1;
     }
 
     blk = allocBlock();
-    if (blk < 0) {
+    if (blk < 0)
+    {
         printf("磁盘已满\n");
         return -1;
     }
@@ -69,7 +78,8 @@ int my_create(char *filename) {
         memcpy(blockaddr[cur->first], &self_fcb, sizeof(fcb));
 
         /* 根目录的 '..' 也指向自己，顺手同步 */
-        if (cur->first == 5) {
+        if (cur->first == 5)
+        {
             fcb parent_fcb = cur->open_fcb;
             strcpy(parent_fcb.filename, "..");
             memcpy(blockaddr[cur->first] + sizeof(fcb), &parent_fcb, sizeof(fcb));
@@ -77,16 +87,18 @@ int my_create(char *filename) {
     }
 
     printf("创建成功\n");
-    return my_open(filename);  /* 创建后直接打开 */
+    return my_open(filename); /* 创建后直接打开 */
 }
 /* ===================== 2. 删除文件 ===================== */
 
-void my_rm(char *filename) {
+void my_rm(char *filename)
+{
     useropen *cur = &openfilelist[curdirid];
     fcb target;
     int index;
 
-    if (find_file(cur->first, cur->open_fcb.length, filename, &target, &index) != 0) {
+    if (find_file(cur->first, cur->open_fcb.length, filename, &target, &index) != 0)
+    {
         printf("文件不存在\n");
         return;
     }
@@ -105,18 +117,33 @@ void my_rm(char *filename) {
 }
 
 /* ===================== 3. 打开文件 ===================== */
-int my_open(char *filename) {
+int my_open(char *filename)
+{
     useropen *cur = &openfilelist[curdirid];
     fcb target;
     int index;
 
-    if (find_file(cur->first, cur->open_fcb.length, filename, &target, &index) != 0) {
+    if (find_file(cur->first, cur->open_fcb.length, filename, &target, &index) != 0)
+    {
         printf("文件不存在\n");
         return -1;
     }
 
+    /* 防止同一个文件被重复打开 */
+    for (int i = 1; i < MAXOPENFILE; i++)
+    {
+        if (openfilelist[i].topenfile == 1 &&
+            openfilelist[i].first == target.first &&
+            openfilelist[i].dirno == cur->first)
+        {
+            printf("文件已经打开，fd = %d\n", i);
+            return i;
+        }
+    }
+
     int fd = getFreeOpenlist();
-    if (fd < 0) {
+    if (fd < 0)
+    {
         printf("打开文件表已满\n");
         return -1;
     }
@@ -134,15 +161,18 @@ int my_open(char *filename) {
 
 /* ===================== 4. 关闭文件 ===================== */
 
-void my_close(int fd) {
-    if (!check_fd(fd)) {
+void my_close(int fd)
+{
+    if (!check_fd(fd))
+    {
         printf("fd非法\n");
         return;
     }
 
     useropen *file = &openfilelist[fd];
 
-    if (file->fcbstate == 1) {
+    if (file->fcbstate == 1)
+    {
         file->open_fcb.length = file->length;
 
         memcpy(blockaddr[file->dirno] + file->diroff,
